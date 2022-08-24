a57b4f4da04bcf4d3194f698a3247fc036a748d8
froehlichA <froehlich.alexander@protonmail.com>
2022-08-24 17:04:38 +0200

    type table correctly

--- packages/core/src/core/table.spec.ts
+++ packages/core/src/core/table.spec.ts
@@ -1,18 +1,34 @@
 import { createDB, SyncDB } from "./createDB";
 import { table } from "./table";
 
+interface User1 {
+  id: string;
+  name: string;
+}
+
+interface User2 {
+  uuid: string;
+  age: number;
+}
+
 let db: SyncDB;
 
 beforeEach(() => {
     db = createDB();
-})
+});
 
 it("should create a table without options", () => {
-  table(db, "user");
+  table<User1>(db, "user")();
 });
 
 it("should create a table with a different primary key", () => {
-  table(db, "user", {
+  table<User1>(db, "user")({
+    primary: "name"
+  });
+  table<User2>(db, "user")({
     primary: "uuid"
+  });
+  table<User2>(db, "user")({
+    primary: "age"
   });
 });
--- packages/core/src/core/table.ts
+++ packages/core/src/core/table.ts
@@ -3,38 +3,95 @@ import { SyncDB, SyncKey } from "./createDB";
 /**
  * Creates a new table where entities can be inserted/updated/deleted/retrieved.
  * 
- * The default primary key of a table is `id`. If you'd like to change that, supply a
- * `primary` property in `options` (either a string, or an array of strings for composite
- * primary keys).
+ * The default primary key of a table is `id`. If your interface does not have
+ * a `id` property or you'd like to change it to something else, use this snippet:
  * 
- * @example
+ * ```
+ * interface User {
+ *   uuid: string;
+ *   name: string;
+ * }
+ * 
  * const db = createDB();
- * const userTable = table<User>(db, "users");
- * const taskTable = table<Task>(db, "tasks", {
- *   primary: "uuid"
+ * const userTable = table<User>(db, "users")({
+ *   primary: "uuid" // whatever you want your primary key to be
  * });
+ * ```
+ * 
+ * Other options can be supplied with the `options` parameter.
+ *
+ * @example
+ * interface User {
+ *   id: string;
+ *   name: string;
+ * }
+ * 
+ * const db = createDB();
+ * const userTable = table<User>(db, "users")();
  */
-export function table<T>(db: SyncDB, tableName: string, options?: TableOptions): SyncTable<T> {
+export function table<T extends { id: string }>(
+  db: SyncDB,
+  tableName: string
+): <P extends keyof T = "id">(options?: TableOptions<P>) => SyncTable<T, P>;
 
-  return {
-    [SyncKey]: {
-      db,
-      tableName,
-      options: {
-        primary: options?.primary ?? ("id" as string),
+/**
+ * Creates a new table where entities can be inserted/updated/deleted/retrieved.
+ * 
+ * The default primary key of a table is `id`. If your interface does not have
+ * a `id` property or you'd like to change it to something else, use this snippet:
+ * 
+ * ```
+ * interface User {
+ *   uuid: string;
+ *   name: string;
+ * }
+ * 
+ * const db = createDB();
+ * const userTable = table<User>(db, "users")({
+ *   primary: "uuid" // whatever you want your primary key to be
+ * });
+ * ```
+ * 
+ * Other options can be supplied with the `options` parameter.
+ *
+ * @example
+ * interface User {
+ *   id: string;
+ *   name: string;
+ * }
+ * 
+ * const db = createDB();
+ * const userTable = table<User>(db, "users")();
+ */
+export function table<T>(
+  db: SyncDB,
+  tableName: string
+): <P extends keyof T>(options: TableOptions<P>) => SyncTable<T, P>;
+
+export function table<T>(db: SyncDB, tableName: string) {
+  return <P extends keyof T>(
+    options: TableOptions<P>
+  ): SyncTable<T, P | "id"> => {
+    return {
+      [SyncKey]: {
+        db,
+        tableName,
+        options: {
+          primary: options?.primary ?? "id",
+        },
       },
-    },
+    };
   };
 }
 
-export interface TableOptions {
-  primary?: string|string[];
+export interface TableOptions<P> {
+  primary: P;
 }
 
-export interface SyncTable<T> {
+export interface SyncTable<T, P> {
   [SyncKey]: {
     db: SyncDB;
     tableName: string;
-    options: Required<TableOptions>;
+    options: Required<TableOptions<P>>;
   };
 }
