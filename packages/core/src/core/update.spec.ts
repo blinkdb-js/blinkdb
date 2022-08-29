baff1e8a3673c3822e60f13826985457a6843fd5
froehlichA <froehlich.alexander@protonmail.com>
2022-08-29 17:52:41 +0200

    implement update

---
+++ packages/core/src/core/update.spec.ts
@@ -0,0 +1,49 @@
+import { create } from "./create";
+import { createDB, SyncDB } from "./createDB";
+import { SyncTable, table } from "./table";
+import { update } from "./update";
+import { one } from "./one";
+
+interface User {
+  id: number;
+  name: string;
+  age?: number;
+}
+
+let db: SyncDB;
+let userTable: SyncTable<User, "id">;
+
+beforeEach(async () => {
+  db = createDB();
+  userTable = table<User>(db, "users")();
+
+  await create(userTable, { id: 0, name: "Alice", age: 16 });
+  await create(userTable, { id: 1, name: "Bob" });
+  await create(userTable, { id: 2, name: "Charlie", age: 49 });
+});
+
+it("should throw if the primary key given to update is not found", async () => {
+  expect(update(userTable, { id: 3 })).rejects.toThrow(
+    /Item with primary key .* not found./
+  );
+});
+
+it("should update the entity if the primary key is found", async () => {
+  await update(userTable, { id: 2, name: "Charlie the II." });
+  const charlie = await one(userTable, { where: { id: 2 } });
+  expect(charlie).toStrictEqual({
+    id: 2,
+    name: "Charlie the II.",
+    age: 49
+  });
+});
+
+it("should update multiple properties", async () => {
+  await update(userTable, { id: 0, name: "Alice the II.", age: 25 });
+  const alice = await one(userTable, { where: { id: 0 } });
+  expect(alice).toStrictEqual({
+    id: 0,
+    name: "Alice the II.",
+    age: 25
+  });
+});
---
+++ packages/core/src/core/update.ts
@@ -0,0 +1,34 @@
+import { SyncKey } from "./createDB";
+import { SyncTable } from "./table";
+
+/**
+ * Saves updates of the given `entity` in `table`.
+ * 
+ * @throws if the entity has not been inserted into the table before, e.g. if the primary key of the entity was not found.
+ * 
+ * @example
+ * const db = createDB();
+ * const userTable = table<User>(db, "users")();
+ * const userId = await create(userTable, { id: uuid(), name: 'Alice', age: 15 });
+ * // Increase the age of Alice
+ * await update(userTable, { id: userId, age: 16 });
+ */
+export async function update<T, P extends keyof T>(table: SyncTable<T, P>, diff: Diff<T, P>): Promise<void> {
+  const primaryKeyProperty = table[SyncKey].options.primary;
+  const primaryKey = String(diff[primaryKeyProperty]);
+  const item = table[SyncKey].storage.primary.get(primaryKey);
+
+  if(!item) {
+    throw new Error(`Item with primary key "${primaryKey}" not found.`);
+  }
+
+  for(let key in diff) {
+    if(key === primaryKeyProperty) {
+      continue;
+    }
+    item[key as keyof T] = diff[key as keyof Diff<T, P>] as any;
+  }
+}
+
+export type Diff<T, P extends keyof T> = Partial<Omit<T, P>> & Required<Pick<T, P>>;
+
