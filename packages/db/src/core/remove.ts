06f8b72f7702551f08603f09aea160c288d71f81
Alexander Fröhlich <froehlich.alexander@protonmail.com>
2022-09-17 13:08:27 +0200

    fix types in remove

--- packages/db/src/core/remove.ts
+++ packages/db/src/core/remove.ts
@@ -20,34 +20,30 @@ export async function remove<T, P extends keyof T>(
 ): Promise<boolean> {
   const primaryKeyProperty = table[BlinkKey].options.primary;
   const primaryKey = entity[primaryKeyProperty];
-  const indexEntries = Object.entries<BTree<any, T[]>>(
-    table[BlinkKey].storage.indexes as any
-  );
-  // For tables without indexes, it is not necessary to retrieve the actual entity before deleting it
-  // For tables with indexes, we need the full entity to find all corresponding index entries
-  const item =
-    indexEntries.length > 0 ? table[BlinkKey].storage.primary.get(primaryKey) : undefined;
-  const deleted = table[BlinkKey].storage.primary.delete(primaryKey);
-  if (item !== undefined) {
-    for (const [property, btree] of indexEntries) {
-      const key = (item as any)[property];
-      if (key == null) continue;
+  
+  const indexes = table[BlinkKey].storage.indexes;
+  if(Object.keys(indexes).length > 0) {
+    const item = table[BlinkKey].storage.primary.get(primaryKey);
+    if(!item) return false;
+    for(const property in indexes) {
+      const btree = indexes[property]!;
+      const key = item[property];
+      if(key === null || key === undefined) continue;
 
-      const items = btree.get(key);
-      if (items !== undefined) {
-        const deleteIndex = items.indexOf(item);
-        if (deleteIndex !== -1) {
-          if (items.length === 1) {
-            btree.delete(key);
-          } else {
-            items.splice(deleteIndex, 1);
-          }
+      const items = btree.get(key)!;
+      const deleteIndex = items.indexOf(item);
+      if (deleteIndex !== -1) {
+        if (items.length === 1) {
+          btree.delete(key);
+        } else {
+          items.splice(deleteIndex, 1);
         }
       }
     }
   }
+
   table[BlinkKey].events.onRemove.dispatch({ entity: entity as unknown as T });
-  return deleted;
+  return table[BlinkKey].storage.primary.delete(primaryKey);
 }
 
-export type Ids<T, P extends keyof T> = Required<Pick<T, P>>;
+export type Ids<T, P extends keyof T> = { [K in P]-?: T[P]; };
