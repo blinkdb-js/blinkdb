30fddf1d4d9d1809a0d7db388f57c6e3e70be668
froehlichA <froehlich.alexander@protonmail.com>
2022-10-07 23:31:24 +0200

    implement filter index

---
+++ packages/db/src/new-query/filter/index.ts
@@ -0,0 +1,17 @@
+import { And, Or, Where } from "../types";
+import { matchesAnd } from "./and";
+import { matchesOr } from "./or";
+import { matchesWhere } from "./where";
+
+/**
+ * @returns whether the given `item` matches the filter.
+ */
+export function matches<T>(item: T, filter: Where<T> | And<T> | Or<T>): boolean {
+  if ("$and" in filter) {
+    return matchesAnd(item, filter);
+  } else if ("$or" in filter) {
+    return matchesOr(item, filter);
+  } else {
+    return matchesWhere(item, filter);
+  }
+}
