0e483870149b0763bb50675389babe7dd6e81fc4
froehlichA <froehlich.alexander@protonmail.com>
2022-11-27 23:50:24 +0100

    rewrite insertMany tests

--- packages/db/src/core/insertMany.spec.ts
+++ packages/db/src/core/insertMany.spec.ts
@@ -1,62 +1,51 @@
+import { generateRandomUsers, User } from "../tests/utils";
 import { createDB, Database } from "./createDB";
 import { createTable, Table } from "./createTable";
 import { insertMany } from "./insertMany";
 import { one } from "./one";
 
-interface User {
-  id: number;
-  name: string;
-  age?: number;
-}
-
-let db: Database;
+let users: User[];
 let userTable: Table<User, "id">;
 
-beforeEach(() => {
-  db = createDB();
-  userTable = createTable<User>(db, "users")();
-});
-
-it("should return the primary key of the inserted item", async () => {
-  const ids = await insertMany(userTable, [{ id: 0, name: "Alice", age: 32 }]);
-  expect(ids).toStrictEqual([0]);
-});
-
-it("should return the primary key of all inserted items", async () => {
-  const ids = await insertMany(userTable, [
-    { id: 0, name: "Alice", age: 32 },
-    { id: 1, name: "Bob", age: 20 },
-  ]);
-  expect(ids).toStrictEqual([0, 1]);
-});
-
-it("should throw if any of the primary keys is not unique", async () => {
-  const fn = async () => {
-    const ids = await insertMany(userTable, [
-      { id: 0, name: "Alice", age: 32 },
-      { id: 0, name: "Bob", age: 20 },
-    ]);
-  };
-  expect(fn).rejects.toThrowError("Primary key 0 already in use");
-});
-
-it("should insert the exact item if db.clone is set to false", async () => {
-  db = createDB({
-    clone: false,
+beforeEach(async () => {
+  users = generateRandomUsers();
+  const db = createDB();
+  userTable = createTable<User>(
+    db,
+    "users"
+  )({
+    primary: "id",
+    indexes: ["name"],
   });
-  userTable = createTable<User>(db, "users")();
-  const user: User = { id: 0, name: "Alice" };
-  await insertMany(userTable, [user]);
-  const item = await one(userTable, { where: { id: 0 } });
-
-  expect(item).toBe(user);
 });
 
-it("should clone the inserted item if db.clone is set to false", async () => {
-  const user: User = { id: 0, name: "Alice" };
-  await insertMany(userTable, [user]);
-  const item = await one(userTable, { where: { id: 0 } });
+it("should return the primary keys of all inserted items", async () => {
+  const ids = await insertMany(userTable, users);
+  expect(ids).toStrictEqual(users.map((u) => u.id));
+});
 
-  expect(item).not.toBe(user);
-  expect(item).toStrictEqual(user);
+it("should insert items retrievable by primary key", async () => {
+  const [aliceId] = await insertMany(userTable, [users[0]]);
+  const user = await one(userTable, { where: { id: aliceId } });
+
+  expect(user).toStrictEqual(users[0]);
+});
+
+it("should insert items retrievable by index", async () => {
+  const aliceId = await insertMany(userTable, [users[0]]);
+  const user = await one(userTable, { where: { name: users[0].name } });
+
+  expect(user).toStrictEqual(users[0]);
+});
+
+it("should insert items retrievable by property", async () => {
+  const aliceId = await insertMany(userTable, [users[0]]);
+  const user = await one(userTable, { where: { age: users[0].age } });
+
+  expect(user).toStrictEqual(users[0]);
+});
+
+it("should prevent duplicate primary keys", async () => {
+  const fn = async () => await insertMany(userTable, [users[0], users[0]]);
+  expect(fn).rejects.toThrowError(`Primary key ${users[0].id} already in use`);
 });
