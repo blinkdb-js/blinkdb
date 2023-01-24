0c71c5789b9ac02502eeac7c6e9966dd7330e408
froehlichA <froehlich.alexander@protonmail.com>
2023-01-24 20:52:56 +0100

    rewrite updateWhere tests

--- packages/db/src/core/updateWhere.spec.ts
+++ packages/db/src/core/updateWhere.spec.ts
@@ -1,41 +1,38 @@
-import { createDB, Database } from "./createDB";
+import { User, generateRandomUsers, sortById } from "../tests/utils";
+import { createDB } from "./createDB";
 import { createTable, Table } from "./createTable";
 import { insertMany } from "./insertMany";
+import { many } from "./many";
 import { one } from "./one";
 import { updateWhere } from "./updateWhere";
 
-interface User {
-  id: number;
-  name: string;
-  age?: number;
-}
-
-let db: Database;
+let users: User[];
 let userTable: Table<User, "id">;
 
 beforeEach(async () => {
-  db = createDB();
-  userTable = createTable<User>(db, "users")();
-
-  await insertMany(userTable, [
-    { id: 0, name: "Alice", age: 16 },
-    { id: 1, name: "Bob" },
-    { id: 2, name: "Charlie", age: 49 },
-  ]);
+  users = generateRandomUsers();
+  const db = createDB();
+  userTable = createTable<User>(
+    db,
+    "users"
+  )({
+    primary: "id",
+    indexes: ["name"],
+  });
+  await insertMany(userTable, users);
 });
 
 it("should throw if the primary key is modified inside the callback", async () => {
-  const fn = async () =>
-    await updateWhere(userTable, { where: { age: 16 } }, (item) => {
-      return { ...item, id: 1 };
-    });
-
-  expect(fn).rejects.toThrow("Primary key cannot be modified in update queries.");
+  expect(
+    updateWhere(userTable, { where: { age: { gt: 0 } } }, (item) => {
+      return { ...item, id: "1" };
+    })
+  ).rejects.toThrow("Primary key cannot be modified in update queries.");
 });
 
 it("should update no items if no items match", async () => {
   const fn = jest.fn();
-  await updateWhere(userTable, { where: { age: 1 } }, fn);
+  await updateWhere(userTable, { where: { age: 1000 } }, fn);
 
   expect(fn.mock.calls.length).toBe(0);
 });
@@ -44,18 +41,24 @@ it("should update items", async () => {
   await updateWhere(userTable, { where: { age: { gte: 0 } } }, (item) => {
     return { ...item, age: item.age ? item.age + 1 : item.age };
   });
+  const oldUsers = users.filter((u) => u.age && u.age > 0);
+  const newUsers = await many(userTable, { where: { age: { gte: 0 } } });
 
-  expect((await one(userTable, { where: { id: 0 } })).age).toBe(17);
-  expect((await one(userTable, { where: { id: 1 } })).age).toBe(undefined);
-  expect((await one(userTable, { where: { id: 2 } })).age).toBe(50);
+  expect(
+    newUsers
+      .map((item) => ({ ...item, age: item.age ? item.age - 1 : item.age }))
+      .sort(sortById)
+  ).toStrictEqual(oldUsers.sort(sortById));
 });
 
 it("should not update items that dont match the filter", async () => {
-  await updateWhere(userTable, { where: { age: 16 } }, (item) => {
-    return { ...item, age: item.age ? item.age + 1 : item.age };
+  const age = users.find((u) => !!u.age)!.age!;
+  await updateWhere(userTable, { where: { age: age } }, (item) => {
+    return { ...item, name: "None" };
+  });
+  const newUsers = await many(userTable, {
+    where: { OR: [{ age: { lt: age } }, { age: { gt: age } }] },
   });
 
-  expect((await one(userTable, { where: { id: 0 } })).age).toBe(17);
-  expect((await one(userTable, { where: { id: 1 } })).age).toBe(undefined);
-  expect((await one(userTable, { where: { id: 2 } })).age).toBe(49);
+  expect(newUsers.map((u) => u.name)).not.toContain("None");
 });
