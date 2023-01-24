3a35937eabc5e7ccb3912829590d08448dd05c60
froehlichA <froehlich.alexander@protonmail.com>
2023-01-24 19:44:07 +0100

    rewrite update tests

--- packages/db/src/core/update.spec.ts
+++ packages/db/src/core/update.spec.ts
@@ -1,67 +1,54 @@
-import { createDB, Database } from "./createDB";
+import { User, generateRandomUsers } from "../tests/utils";
+import { createDB } from "./createDB";
 import { createTable, Table } from "./createTable";
 import { insert } from "./insert";
+import { insertMany } from "./insertMany";
 import { one } from "./one";
 import { update } from "./update";
 
-interface User {
-  id: number;
-  name: string;
-  age?: number;
-}
-
-let db: Database;
+let users: User[];
 let userTable: Table<User, "id">;
 
 beforeEach(async () => {
-  db = createDB();
-  userTable = createTable<User>(db, "users")();
-
-  await insert(userTable, { id: 0, name: "Alice", age: 16 });
-  await insert(userTable, { id: 1, name: "Bob" });
-  await insert(userTable, { id: 2, name: "Charlie", age: 49 });
-});
-
-it("should throw if the primary key given to update is not found", async () => {
-  expect(update(userTable, { id: 3 })).rejects.toThrow(
-    /Item with primary key .* not found./
-  );
-});
-
-it("should update the entity if the primary key is found", async () => {
-  await update(userTable, { id: 2, name: "Charlie the II." });
-  const charlie = await one(userTable, { where: { id: 2 } });
-  expect(charlie).toStrictEqual({
-    id: 2,
-    name: "Charlie the II.",
-    age: 49,
-  });
-});
-
-it("should update multiple properties", async () => {
-  await update(userTable, { id: 0, name: "Alice the II.", age: 25 });
-  const alice = await one(userTable, { where: { id: 0 } });
-  expect(alice).toStrictEqual({
-    id: 0,
-    name: "Alice the II.",
-    age: 25,
-  });
-});
-
-it("should update indexes", async () => {
+  users = generateRandomUsers();
+  const db = createDB();
   userTable = createTable<User>(
     db,
     "users"
   )({
     primary: "id",
-    indexes: ["age", "name"],
+    indexes: ["name"],
   });
+  await insertMany(userTable, users);
+});
 
-  await insert(userTable, { id: 0, name: "Alice", age: 16 });
-  await update(userTable, { id: 0, name: "Alice the II.", age: 25 });
+it("should throw if the primary key given to update is not found", async () => {
+  expect(update(userTable, { id: "1000" })).rejects.toThrow(
+    /Item with primary key .* not found./
+  );
+});
+
+it("should update the entity if the primary key is found", async () => {
+  await update(userTable, { id: "2", name: "Charlie the II." });
+  const charlie = await one(userTable, { where: { id: "2" } });
+
+  expect(charlie.name).toBe("Charlie the II.");
+});
+
+it("should update multiple properties", async () => {
+  await update(userTable, { id: "2", name: "Charlie the II.", age: 25 });
+  const charlie = await one(userTable, { where: { id: "2" } });
+
+  expect(charlie.name).toBe("Charlie the II.");
+  expect(charlie.age).toBe(25);
+});
+
+it("should update indexes", async () => {
+  await update(userTable, { id: "0", name: "Alice the II.", age: 25 });
   const alice = await one(userTable, { where: { name: "Alice the II.", age: 25 } });
+
   expect(alice).toStrictEqual({
-    id: 0,
+    id: "0",
     name: "Alice the II.",
     age: 25,
   });
