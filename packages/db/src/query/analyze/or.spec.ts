7fdfcd9f4202b4e370098c61e29bdfe985d430a3
froehlichA <froehlich.alexander@protonmail.com>
2023-01-25 15:33:44 +0100

    rewrite analyze/or tests

--- packages/db/src/query/analyze/or.spec.ts
+++ packages/db/src/query/analyze/or.spec.ts
@@ -1,63 +1,62 @@
-import { BlinkKey, createDB, createTable, insert, Table } from "../../core";
+import { BlinkKey, createDB, createTable, insertMany, Table } from "../../core";
+import { User, generateRandomUsers } from "../../tests/utils";
 import { analyzeMatcher } from "./matchers";
 import { analyzeOr } from "./or";
 
-interface User {
-  id: string;
-  name: string;
-  age: number;
-}
+let users: User[];
+let userTable: Table<User, "id">;
 
-let table: Table<User, "id">;
-
-beforeAll(async () => {
+beforeEach(async () => {
+  users = generateRandomUsers();
   const db = createDB();
-  table = createTable<User>(
+  userTable = createTable<User>(
     db,
     "users"
   )({
     primary: "id",
     indexes: ["age"],
   });
-  for (let i = 0; i < 100; i++) {
-    await insert(table, { id: String(i), name: "Name", age: i % 100 });
-  }
+  await insertMany(userTable, users);
 });
 
 it("should return 0 if and is empty", () => {
-  expect(analyzeOr(table, { OR: [] })).toBe(0);
+  expect(analyzeOr(userTable, { OR: [] })).toBe(0);
 });
 
 it("should return the complexity of the matcher if only one is specified (primary)", () => {
-  const andResult = analyzeOr(table, { OR: [{ id: { gt: "abc" } }] });
-  const matcherResult = analyzeMatcher(table[BlinkKey].storage.primary, {
+  const andResult = analyzeOr(userTable, { OR: [{ id: { gt: "abc" } }] });
+  const matcherResult = analyzeMatcher(userTable[BlinkKey].storage.primary, {
     gt: "abc",
   });
-  expect(andResult).toBe(matcherResult);
+
+  expect(matcherResult).toBe(andResult);
 });
 
 it("should return the complexity of the matcher if only one is specified (index)", () => {
-  const andResult = analyzeOr(table, { OR: [{ age: { gt: 2 } }] });
-  const matcherResult = analyzeMatcher(table[BlinkKey].storage.indexes["age"]!, {
+  const andResult = analyzeOr(userTable, { OR: [{ age: { gt: 2 } }] });
+  const matcherResult = analyzeMatcher(userTable[BlinkKey].storage.indexes["age"]!, {
     gt: 2,
   });
-  expect(andResult).toBe(matcherResult);
+
+  expect(matcherResult).toBeLessThanOrEqual(andResult);
 });
 
 it("should return Number.MAX_SAFE_INTEGER if a key without index is specified", () => {
-  const andResult = analyzeOr(table, { OR: [{ name: { gt: "abc" } }] });
+  const andResult = analyzeOr(userTable, { OR: [{ name: { gt: "abc" } }] });
+
   expect(andResult).toBe(Number.MAX_SAFE_INTEGER);
 });
 
 it("should return the sum of all matchers if more than one is specified", () => {
-  const andResult = analyzeOr(table, {
+  const andResult = analyzeOr(userTable, {
     OR: [{ id: { lt: "10" } }, { age: { lt: 0 } }],
   });
-  const matcherResult1 = analyzeMatcher(table[BlinkKey].storage.primary, {
+  const matcherResult1 = analyzeMatcher(userTable[BlinkKey].storage.primary, {
     lt: "10",
   });
-  const matcherResult2 = analyzeMatcher(table[BlinkKey].storage.indexes["age"]!, {
+  const matcherResult2 = analyzeMatcher(userTable[BlinkKey].storage.indexes["age"]!, {
     lt: 0,
   });
-  expect(andResult).toBe(matcherResult1 + matcherResult2);
+
+  expect(matcherResult1 + matcherResult2).toBeLessThanOrEqual(andResult);
 });
