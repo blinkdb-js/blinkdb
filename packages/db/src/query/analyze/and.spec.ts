9aa6f90969b6ca783c616c4cfe1ae6419df7eb34
froehlichA <froehlich.alexander@protonmail.com>
2023-01-25 15:30:49 +0100

    rewrite analyze/and tests

--- packages/db/src/query/analyze/and.spec.ts
+++ packages/db/src/query/analyze/and.spec.ts
@@ -1,60 +1,59 @@
-import { BlinkKey, createDB, createTable, insert, Table } from "../../core";
+import { BlinkKey, createDB, createTable, insertMany, Table } from "../../core";
+import { User, generateRandomUsers } from "../../tests/utils";
 import { analyzeAnd } from "./and";
 import { analyzeMatcher } from "./matchers";
 
-interface User {
-  id: string;
-  name: string;
-  age: number;
-}
+let users: User[];
+let userTable: Table<User, "id">;
 
-let table: Table<User, "id">;
-
-beforeAll(async () => {
+beforeEach(async () => {
+  users = generateRandomUsers();
   const db = createDB();
-  table = createTable<User>(
+  userTable = createTable<User>(
     db,
     "users"
   )({
     primary: "id",
     indexes: ["age"],
   });
-  for (let i = 0; i < 100; i++) {
-    await insert(table, { id: String(i), name: "Name", age: i % 100 });
-  }
+  await insertMany(userTable, users);
 });
 
 it("should return 0 if and is empty", () => {
-  expect(analyzeAnd(table, { AND: [] })).toBe(0);
+  expect(analyzeAnd(userTable, { AND: [] })).toBe(0);
 });
 
 it("should return the complexity of the matcher if only one is specified (primary)", () => {
-  const andResult = analyzeAnd(table, { AND: [{ id: { gt: "abc" } }] });
-  const matcherResult = analyzeMatcher(table[BlinkKey].storage.primary, {
+  const andResult = analyzeAnd(userTable, { AND: [{ id: { gt: "abc" } }] });
+  const matcherResult = analyzeMatcher(userTable[BlinkKey].storage.primary, {
     gt: "abc",
   });
-  expect(andResult).toBe(matcherResult);
+
+  expect(matcherResult).toBe(andResult);
 });
 
 it("should return the complexity of the matcher if only one is specified (index)", () => {
-  const andResult = analyzeAnd(table, { AND: [{ age: { gt: 2 } }] });
-  const matcherResult = analyzeMatcher(table[BlinkKey].storage.indexes["age"]!, {
+  const andResult = analyzeAnd(userTable, { AND: [{ age: { gt: 2 } }] });
+  const matcherResult = analyzeMatcher(userTable[BlinkKey].storage.indexes["age"]!, {
     gt: 2,
   });
-  expect(andResult).toBe(matcherResult);
+
+  expect(matcherResult).toBeLessThan(andResult);
 });
 
 it("should return Number.MAX_SAFE_INTEGER if a key without index is specified", () => {
-  const andResult = analyzeAnd(table, { AND: [{ name: { gt: "abc" } }] });
+  const andResult = analyzeAnd(userTable, { AND: [{ name: { gt: "abc" } }] });
+
   expect(andResult).toBe(Number.MAX_SAFE_INTEGER);
 });
 
 it("should return the better matcher if more than one is specified", () => {
-  const andResult = analyzeAnd(table, {
+  const andResult = analyzeAnd(userTable, {
     AND: [{ id: { lt: "10" } }, { age: { lt: 0 } }],
   });
-  const matcherResult = analyzeMatcher(table[BlinkKey].storage.indexes["age"]!, {
+  const matcherResult = analyzeMatcher(userTable[BlinkKey].storage.indexes["age"]!, {
     lt: 0,
   });
-  expect(andResult).toBe(matcherResult);
+
+  expect(matcherResult).toBeLessThan(andResult);
 });
