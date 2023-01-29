e3ff1f46011f988fae995b25e3270f5004512453
froehlichA <froehlich.alexander@protonmail.com>
2023-01-29 15:03:59 +0100

    replace middleware class with function

--- packages/db/src/events/Middleware.spec.ts
+++ packages/db/src/events/Middleware.spec.ts
@@ -1,20 +1,10 @@
-import { Middleware } from "./Middleware";
-import { HookAction, HookContext, HookParams } from "./types";
+import { executeHooks } from "./Middleware";
+import { Hook, HookAction, HookContext } from "./types";
 import { User } from "../tests/utils";
-
-let middleware: Middleware<User, "id">;
-
-beforeEach(() => {
-  middleware = new Middleware();
-});
-
-it("should allow registering hooks", () => {
-  middleware.register(({ next }) => next());
-});
 
 it("should work without hooks", async () => {
   const context = {} as HookContext<User, "id", "count">;
-  const result = await middleware.run(context, () => 123);
+  const result = executeHooks(context, [] as Hook<User, "id">[], () => 123);
 
   expect(result).toBe(123);
 });
@@ -23,8 +13,8 @@ describe("hooks", () => {
   it("should work", async () => {
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(({ next }) => next());
-    const result = await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [({ next }) => next()];
+    const result = executeHooks(context, hooks, () => 123);
 
     expect(result).toBe(123);
   });
@@ -42,13 +32,15 @@ describe("hooks", () => {
       action: "count",
     } as HookContext<User, "id", "count">;
 
-    middleware.register(async (ctx) => {
-      if (isAction(ctx, "count")) {
-        const num = await ctx.next();
-        return num + 1;
-      }
-    });
-    const result = await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [
+      async (ctx) => {
+        if (isAction(ctx, "count")) {
+          const num = await ctx.next();
+          return num + 1;
+        }
+      },
+    ];
+    const result = await executeHooks(context, hooks, () => 123);
 
     expect(result).toBe(124);
   });
@@ -56,10 +48,8 @@ describe("hooks", () => {
   it("should be able to return new values", async () => {
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(() => {
-      return 321;
-    });
-    const result = await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [() => 321];
+    const result = await executeHooks(context, hooks, () => 123);
 
     expect(result).toBe(321);
   });
@@ -67,10 +57,12 @@ describe("hooks", () => {
   it("should be able to throw", async () => {
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(() => {
-      throw new Error();
-    });
-    const fn = async () => await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [
+      () => {
+        throw new Error();
+      },
+    ];
+    const fn = async () => await executeHooks(context, hooks, () => 123);
 
     expect(fn).rejects.toThrowError();
   });
@@ -79,10 +71,8 @@ describe("hooks", () => {
     const fn = jest.fn(() => 123);
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(() => {
-      return 321;
-    });
-    await middleware.run(context, fn);
+    const hooks: Hook<User, "id">[] = [() => 123];
+    const result = await executeHooks(context, hooks, fn);
 
     expect(fn).toHaveBeenCalledTimes(0);
   });
@@ -91,13 +81,15 @@ describe("hooks", () => {
     const fn = jest.fn(() => 123);
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(({ next }) => {
-      for (let i = 0; i < 10; i++) {
-        next();
-      }
-      return 321;
-    });
-    await middleware.run(context, fn);
+    const hooks: Hook<User, "id">[] = [
+      ({ next }) => {
+        for (let i = 0; i < 10; i++) {
+          next();
+        }
+        return 321;
+      },
+    ];
+    const result = await executeHooks(context, hooks, fn);
 
     expect(fn).toHaveBeenCalledTimes(10);
   });
@@ -107,25 +99,27 @@ describe("hooks", () => {
       action: "count",
     } as HookContext<User, "id", "count">;
 
-    middleware.register(async (ctx) => {
-      if (isAction(ctx, "count")) {
-        const num = await ctx.next();
-        return num + 100;
-      }
-    });
-    middleware.register(async (ctx) => {
-      if (isAction(ctx, "count")) {
-        const num = await ctx.next();
-        return num + 10;
-      }
-    });
-    middleware.register(async (ctx) => {
-      if (isAction(ctx, "count")) {
-        const num = await ctx.next();
-        return num + 1;
-      }
-    });
-    const result = await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [
+      async (ctx) => {
+        if (isAction(ctx, "count")) {
+          const num = await ctx.next();
+          return num + 100;
+        }
+      },
+      async (ctx) => {
+        if (isAction(ctx, "count")) {
+          const num = await ctx.next();
+          return num + 10;
+        }
+      },
+      async (ctx) => {
+        if (isAction(ctx, "count")) {
+          const num = await ctx.next();
+          return num + 1;
+        }
+      },
+    ];
+    const result = await executeHooks(context, hooks, () => 123);
 
     expect(result).toBe(234);
   });
@@ -133,10 +127,8 @@ describe("hooks", () => {
   it("should be called in reverse order", async () => {
     const context = {} as HookContext<User, "id", "count">;
 
-    middleware.register(() => 111);
-    middleware.register(() => 222);
-    middleware.register(() => 333);
-    const result = await middleware.run(context, () => 123);
+    const hooks: Hook<User, "id">[] = [() => 111, () => 222, () => 333];
+    const result = await executeHooks(context, hooks, () => 123);
 
     expect(result).toBe(111);
   });
--- packages/db/src/events/Middleware.ts
+++ packages/db/src/events/Middleware.ts
@@ -1,31 +1,26 @@
-import { Hook, HookContext, HookReturn } from "./types";
+import { Hook, HookAction, HookContext, HookReturn } from "./types";
 
-export class Middleware<T extends object = any, P extends keyof T = keyof T> {
-  private hooks: Hook<T, P>[] = [];
-
-  public register(hook: Hook<T, P>): () => void {
-    const index = this.hooks.push(hook);
-    return () => this.hooks.splice(index, 1);
-  }
-
-  public run(
-    context: Omit<HookContext<T, P>, "next">,
-    impl: () => HookReturn<T, P> | Promise<HookReturn<T, P>>
-  ): HookReturn<T, P> | Promise<HookReturn<T, P>> {
-    return this.next(context, this.hooks, impl);
-  }
-
-  private next(
-    context: Omit<HookContext<T, P>, "next">,
-    hooks: Hook<T, P>[],
-    impl: () => HookReturn<T, P> | Promise<HookReturn<T, P>>
-  ): HookReturn<T, P> | Promise<HookReturn<T, P>> {
-    const [step, ...next] = hooks;
-    return step
-      ? step({
-          ...context,
-          next: () => this.next(context, next, impl),
-        })
-      : impl();
-  }
+/**
+ * Execute all hooks.
+ *
+ * @param context the context to supply to the hooks.
+ * @param hooks the list of hooks to execute. The first item in the list will be executed first.
+ * @param impl the implementation that will be called if the last hook calls `next()`.
+ */
+export function executeHooks<
+  T extends object = any,
+  P extends keyof T = keyof T,
+  A extends HookAction = HookAction
+>(
+  context: Omit<HookContext<T, P, A>, "next">,
+  hooks: Hook<T, P, A>[],
+  impl: () => HookReturn<T, P, A> | Promise<HookReturn<T, P, A>>
+): HookReturn<T, P, A> | Promise<HookReturn<T, P, A>> {
+  const [step, ...next] = hooks;
+  return step
+    ? step({
+        ...context,
+        next: () => executeHooks(context, next, impl),
+      })
+    : impl();
 }
