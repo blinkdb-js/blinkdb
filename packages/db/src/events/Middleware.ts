f119abe3f70431dc0bd899fe9700a03bab12cf66
froehlichA <froehlich.alexander@protonmail.com>
2023-01-30 20:27:11 +0100

    add function for executing hooks from table methods

--- packages/db/src/events/Middleware.ts
+++ packages/db/src/events/Middleware.ts
@@ -1,3 +1,4 @@
+import { BlinkKey, Table } from "../core";
 import { Hook, HookAction, HookContext, HookReturn } from "./types";
 
 /**
@@ -23,4 +24,34 @@ export function executeHooks<
         next: () => executeHooks(context, next, impl),
       })
     : impl();
+}
+
+/**
+ * Execute all hooks for a given table (and its database).
+ * Hooks will be loaded from the database of the table + the table itself.
+ *
+ * @param table the table to execute hooks for.
+ * @param context the context to supply to the hooks.
+ * @param impl the implementation that will be called if the last hook calls `next()`.
+ */
+export function executeTableHooks<
+  T extends object = any,
+  P extends keyof T = keyof T,
+  A extends HookAction = HookAction
+>(
+  table: Table<T, P>,
+  context: Omit<Parameters<typeof executeHooks<T, P, A>>[0], "table">,
+  impl: Parameters<typeof executeHooks<T, P, A>>[2]
+): ReturnType<typeof executeHooks<T, P, A>> {
+  const dbHooks = table[BlinkKey].db[BlinkKey].hooks;
+  const tableHooks = table[BlinkKey].hooks;
+
+  return executeHooks(
+    {
+      ...context,
+      table: table[BlinkKey].tableName,
+    },
+    [...dbHooks, ...tableHooks],
+    impl
+  );
 }
