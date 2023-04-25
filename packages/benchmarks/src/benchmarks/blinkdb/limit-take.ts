942600a8dfae2d6972be83c4095575d93196d069
froehlichA <froehlich.alexander@protonmail.com>
2023-04-26 01:03:45 +0200

    add limit-take benchmark

---
+++ packages/benchmarks/src/benchmarks/blinkdb/limit-take.ts
@@ -0,0 +1,39 @@
+import { randFirstName } from "@ngneat/falso";
+import { createDB, createTable, insertMany, many } from "blinkdb";
+import loki from "lokijs";
+import { Bench } from "tinybench";
+
+interface User {
+  id: number;
+  name: string;
+  age?: number;
+}
+
+const blinkdb = createDB({ clone: false });
+let blinkUserTable = createTable<User>(blinkdb, "users")();
+
+const lokidb = new loki("users.db");
+let lokiUserTable = lokidb.addCollection<User>("users", { unique: ["id"] });
+
+let users: User[] = [];
+for (let i = 0; i < 10000; i++) {
+  users.push({
+    id: i,
+    name: randFirstName(),
+    age: Math.random() > 0.2 ? Math.random() * 40 : undefined,
+  });
+}
+
+lokiUserTable.insert(users);
+insertMany(blinkUserTable, users);
+
+export const bench = new Bench()
+  .add("lokijs", () => {
+    lokiUserTable
+      .chain()
+      .find({ id: { $gt: 9000 } })
+      .limit(100);
+  })
+  .add("blinkdb", async () => {
+    await many(blinkUserTable, { limit: { from: 9000, take: 100 } });
+  });
