e566c070548a71453ff4737ec5936d3c91fa971f
froehlichA <froehlich.alexander@protonmail.com>
2023-04-26 16:47:55 +0200

    implement sort benchmarks on non-primary keys

---
+++ packages/benchmarks/src/benchmarks/blinkdb/sort-age-asc.ts
@@ -0,0 +1,36 @@
+import { randFirstName } from "@ngneat/falso";
+import { createDB, createTable, insertMany, many } from "blinkdb";
+import loki from "lokijs";
+import { Bench } from "tinybench";
+
+interface User {
+  id: number;
+  name: string;
+  age?: number;
+}
+
+const blinkdb = createDB({ clone: false });
+let blinkUserTable = createTable<User>(blinkdb, "users")();
+
+const lokidb = new loki("users.db");
+let lokiUserTable = lokidb.addCollection<User>("users", { unique: ["id"] });
+
+let users: User[] = [];
+for (let i = 0; i < 10000; i++) {
+  users.push({
+    id: i,
+    name: randFirstName(),
+    age: Math.random() > 0.2 ? Math.random() * 40 : undefined,
+  });
+}
+
+lokiUserTable.insert(users);
+insertMany(blinkUserTable, users);
+
+export const bench = new Bench({ iterations: 1 })
+  .add("lokijs", () => {
+    lokiUserTable.chain().find().simplesort("age");
+  })
+  .add("blinkdb", async () => {
+    await many(blinkUserTable, { sort: { key: "age", order: "asc" } });
+  });
