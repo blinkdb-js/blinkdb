d4698a3559245f2b82f5a838ce1acfd9677a95c7
froehlicha <froehlich.alexander@protonmail.com>
2023-04-29 18:20:56 +0200

    implement upsert & upsert-many benchmarks

---
+++ packages/benchmarks/src/benchmarks/blinkdb/upsert.ts
@@ -0,0 +1,43 @@
+import { randFirstName } from "@ngneat/falso";
+import { createDB, createTable, insertMany, upsert } from "blinkdb";
+import loki from "lokijs";
+import { Bench } from "tinybench";
+
+interface User {
+  id: number;
+  name: string;
+  age?: number;
+}
+
+const blinkdb = createDB({ clone: false });
+let blinkUserTable = createTable<User>(blinkdb, "users")();
+let blinkIndex = 0;
+
+const lokidb = new loki("users.db");
+let lokiUserTable = lokidb.addCollection<User>("users", { unique: ["id"] });
+let lokiIndex = 0;
+
+let users: User[] = [];
+for (let i = 0; i < 100000; i++) {
+  users.push({
+    id: i,
+    name: randFirstName(),
+    age: Math.random() > 0.2 ? Math.random() * 40 : undefined,
+  });
+}
+
+lokiUserTable.insert(users.slice(0, 50000));
+insertMany(blinkUserTable, users.slice(0, 50000));
+
+export const bench = new Bench()
+  .add("lokijs", () => {
+    const user = users[lokiIndex++];
+    if(lokiUserTable.get((user as any).$loki)) {
+     lokiUserTable.update(user);
+    } else {
+      lokiUserTable.insert(user);
+    }
+  })
+  .add("blinkdb", async () => {
+    await upsert(blinkUserTable, users[blinkIndex++]);
+  });
