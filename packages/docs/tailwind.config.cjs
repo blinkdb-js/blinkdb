67adf33292b2a1c3086bf2168e3ac641e22c1e98
froehlichA <froehlich.alexander@protonmail.com>
2023-05-05 16:28:08 +0200

    remove all external fonts

--- packages/docs/tailwind.config.cjs
+++ packages/docs/tailwind.config.cjs
@@ -3,11 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,astro,js,ts,md,mdx}"],
   theme: {
-    fontFamily: {
-      display: ["Fredoka", ...defaultTheme.fontFamily.sans],
-      sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
-    },
     extend: {
       animation: {
         extendwidth: "extendwidth 1s ease-in-out",
