528deaae8944a73cf4b1d4065656e51c09058030
froehlichA <froehlich.alexander@protonmail.com>
2023-05-10 23:56:45 +0200

    factor out bg dot backgrounds

--- packages/docs/tailwind.config.cjs
+++ packages/docs/tailwind.config.cjs
@@ -1,18 +1,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,astro,js,ts,md,mdx}"],
   theme: {
     extend: {
-      animation: {
-        extendwidth: "extendwidth 1s ease-in-out",
+      backgroundImage: {
+        "dot-top": "linear-gradient(to top, var(--tw-gradient-from), var(--tw-gradient-from) min(30%, 6em), transparent), radial-gradient(#777 0.65px, var(--tw-gradient-to) 0.65px)",
+        "dot-bottom": "linear-gradient(to bottom, var(--tw-gradient-from), var(--tw-gradient-from) min(30%, 6em), transparent), radial-gradient(#777 0.65px, var(--tw-gradient-to) 0.65px)"
       },
-      keyframes: {
-        extendwidth: {
-          "0%": { width: "0%" },
-          "100%": { width: "100%" },
-        },
-      },
+      backgroundSize: {
+        "dot-top": "cover, 13px 13px",
+        "dot-bottom": "cover, 13px 13px"
+      }
     },
   },
 };
