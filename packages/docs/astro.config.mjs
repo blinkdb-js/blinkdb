9608eeea808dfcb4fc94e4367291121bdf71133c
froehlichA <froehlich.alexander@protonmail.com>
2023-05-12 01:20:16 +0200

    add prefetching to docs

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,11 +1,16 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    prefetch({ selector: "a[href^='/docs']" })
+  ],
   markdown: {
     shikiConfig: {
       theme: "poimandres",
