727487b799dc5adb90c39e2aa0c23048d5610888
froehlicha <froehlich.alexander@protonmail.com>
2023-05-20 16:37:22 +0200

    implement useFirst.ts

--- packages/react/src/retrieval/index.ts
+++ packages/react/src/retrieval/index.ts
@@ -1,2 +1,3 @@
+export * from './useFirst';
 export * from './useMany';
 export * from './types';
---
+++ packages/react/src/retrieval/useFirst.ts
@@ -0,0 +1,61 @@
+import { Entity, key, PrimaryKeyOf, Query, Table } from "blinkdb";
+import { QueryResult } from "./types";
+import { useMany } from "./useMany";
+
+/**
+ * Retrieves the first entity from `table`.
+ *
+ * @example
+ * // Retrieve the first user
+ * const { data: firstUser } = await useFirst(userTable);
+ */
+export function useFirst<T extends Entity<T>, P extends PrimaryKeyOf<T>>(
+  table: Table<T, P>
+): QueryResult<T | null>;
+
+/**
+ * Retrieves the first entity from `table` matching the given `filter`.
+ *
+ * @example
+ * // Retrieve the first user named 'Alice'
+ * const { data: firstUser } = await useFirst(userTable, {
+ *   where: {
+ *     name: "Alice"
+ *   }
+ * });
+ */
+export function useFirst<T extends Entity<T>, P extends PrimaryKeyOf<T>>(
+  table: Table<T, P>,
+  query: Query<T, P>
+): QueryResult<T | null>;
+
+/**
+ * Retrieves the first entity from `table` with the given `id`.
+ *
+ * @example
+ * // Retrieve the 'Alice' user by their id
+ * const { data: firstUser } = await useFirst(userTable, 'alice-uuid');
+ */
+export function useFirst<T extends Entity<T>, P extends PrimaryKeyOf<T>>(
+  table: Table<T, P>,
+  id: T[P]
+): QueryResult<T | null>;
+
+export function useFirst<T extends Entity<T>, P extends PrimaryKeyOf<T>>(
+  table: Table<T, P>,
+  queryOrId?: Query<T, P> | T[P]
+): QueryResult<T | null> {
+  const primaryKeyProperty = key(table);
+  let result: QueryResult<T[]>;
+  if(queryOrId === undefined) {
+    result = useMany(table);
+  } else {
+    const query = typeof queryOrId === "object" ? queryOrId : { where: { [primaryKeyProperty]: queryOrId } } as unknown as Query<T, P>;
+    result = useMany(table, query);
+  }
+
+  return {
+    ...result,
+    data: result.data ? (result.data[0] ?? null) : undefined
+  } as QueryResult<T | null>;
+}
