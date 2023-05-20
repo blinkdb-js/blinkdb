620bd6714a010e5ea173053907ed56a0b5acddba
froehlicha <froehlich.alexander@protonmail.com>
2023-05-20 18:25:22 +0200

    add error state

--- packages/react/src/retrieval/types.ts
+++ packages/react/src/retrieval/types.ts
@@ -1,14 +1,22 @@
 /**
  * Result returned from a call to `useMany()`.
  */
-export type QueryResult<T> = LoadingQueryResult<T> | DoneQueryResult<T>;
+export type QueryResult<T> = LoadingQueryResult<T> | DoneQueryResult<T> | ErrorQueryResult<T>;
 
 interface LoadingQueryResult<T> {
   data: undefined;
+  error: undefined;
   state: "loading";
 }
 
 interface DoneQueryResult<T> {
   data: T;
+  error: undefined;
   state: "done";
+}
+
+interface ErrorQueryResult<T> {
+  data: undefined;
+  error: Error;
+  state: "error";
 }
